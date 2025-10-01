cb3363189f411263de7310281de6f54dab65def0
Aloys Bernard <aloys.bernard@gmail.com>
2025-10-01 14:42:48 +0000

    Refactor some code

---
+++ models/dim_customer.sql
@@ -0,0 +1,9 @@
+select 
+    customer_id,
+    min(order_date) as first_order_date,
+    max(order_date) as most_recent_order_date,
+    count(order_id) as number_of_orders
+
+
+    FROM {{ ref("stg_orders")}}
+    group by 1
--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,3 +1,6 @@
-select id as order_id, user_id as customer_id, order_date, status
-
-from {{ source("jaffle_shop", "orders") }}
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from dbt-tutorial.jaffle_shop.orders
